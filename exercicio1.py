e62847baaf09d805e548a39f01c6290ed63d4d42
abreu <141186203+aabreu6@users.noreply.github.com>
2024-12-13 10:51:37 -0300

    Add files via upload

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,12 @@
-str = "ABACAXI"
+str = "Hoje é um bom dia"
+substr1 = str[0:4] #hoje
+substr2 = str[5] #é
+substr3 = str[7:9] #um
+substr4 = str[10:13] #bom
+substr5= str[14:17] #dia
 
-for i in range(7):
-    print(str[i])
+print(substr1)
+print(substr2)
+print(substr3)
+print(substr4)
+print(substr5)

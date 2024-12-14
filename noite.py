a03cd9910565af12b8e168bf047d750082c913eb
abreu <141186203+aabreu6@users.noreply.github.com>
2024-12-14 19:23:44 -0300

    exc.py

---
+++ noite.py
@@ -0,0 +1,13 @@
+str = "Hoje é um bom dia"
+substr1 = str[0:4] #Hoje
+substr2 = str[5] #É
+substr3 = str[7:9] #Um
+substr4 = str[10:13] #Bom
+substr5= str[14:17] #Dia
+
+print(substr1)
+print(substr2)
+print(substr3)
+print(substr4)
+print(substr5)
+
